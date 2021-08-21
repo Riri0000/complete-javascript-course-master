9aac7e1593562fe5d2f0dc416407eee61ed93696
Riri0000 <ririko010102@gmail.com>
2021-08-21 15:39:59 +0900

    Add: finish ch33

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hasDriversLicnese = false;
+let passTest = true;
+
+if (passTest) hasDriversLicnese = true;
+if (hasDriversLicnese) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534;
