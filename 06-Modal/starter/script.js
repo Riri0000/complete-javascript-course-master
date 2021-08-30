db9cebfed4c899ff504414649e8ab7f340a5c65b
Riri0000 <ririko010102@gmail.com>
2021-08-30 21:02:24 +0900

    Add: finish ch80

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -8,16 +8,24 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-}
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-}
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', closeModal)
+btnCloseModal.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
