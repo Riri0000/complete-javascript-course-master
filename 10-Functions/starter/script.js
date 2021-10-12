533a0f54926a25a836b3c86463627943b97d089c
Riri0000 <ririko010102@gmail.com>
2021-10-12 21:24:22 +0900

    Add: finish ch130

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,3 +24,62 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schemedtmann',
+  passport: 2141212412,
+};
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 2141212412) {
+//     alert('Checked in')
+//   } else {
+//     alert('Wrong passport!')
+//   }
+// }
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // Is the same as doing
+// const flightNum = flight;
+// const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000)
+// }
+
+// newPassport(jonas)
+// checkIn(flight, jonas)
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  // function has name property
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaSctipr is the best!', upperFirstWord);
+transformer('JavaSctipr is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('*+*');
+};
+document.body.addEventListener('click', high5);
